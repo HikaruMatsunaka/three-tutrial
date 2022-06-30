6f9198b3d3823729109904c983fd48b3f9ef5d7d
hikarumatsunaka <mattu.h1103@gmail.com>
2022-06-30 19:03:04 +0900

    updated.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//シーンを用意
+//シーンを作る
+const scene = new THREE.Scene();
+
+//カメラを用意する
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+//レンダリングの準備をする
+const renderer = THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//準備完了
+//描画するものを用意
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+//キューブをシーンに追加する
+scene.add(cube);
+
+//カメラの位置を調整
+camera.position.z = 5;
+
+//最後に、レンダリングをして描画する
